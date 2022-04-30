02889d2f98097ca16cb1e3577e59b2a91093be13
Wesley Augusto Isotton <wesleya.isotton@gmail.com>
2022-04-30 17:50:55 -0300

    use hash as default path on mount

--- src/Explorer.tsx
+++ src/Explorer.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import GridView from "./grid/GridView";
 import PathContext, { useNewPathContext } from "./path/PathContext";
 import sample from "./sample.json";
 
 function Explorer() {
   const pathContext = useNewPathContext();
+
+  useEffect(() => {
+    const hash = window.location.hash.substring(1);
+    if (!hash.startsWith("$")) return;
+
+    pathContext.setPath(hash);
+  }, []);
 
   return (
     <PathContext.Provider value={pathContext}>
