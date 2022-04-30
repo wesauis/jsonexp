2e08388bb1ed36599528caa2085a25ec378bbcfd
Wesley Augusto Isotton <wesleya.isotton@gmail.com>
2022-04-30 15:39:31 -0300

    update scalar types/styles

--- src/grid/GridScalar.tsx
+++ src/grid/GridScalar.tsx
@@ -5,14 +5,17 @@ interface GridScalarProps {
   value: any;
 }
 
-function typeClassOf(value: string) {
-  return value === null ? "null" : typeof value;
-}
-
 const GridScalar: FC<GridScalarProps> = ({ value }) => {
-  const typeClass = "GridScalar " + typeClassOf(value);
+  const typeClass = typeClassOf(value);
 
   return <span className={typeClass}>{String(value)}</span>;
 };
+
+/** JSON "Scalar" Types */
+export type TypeClass = "null" | "string" | "number" | "boolean";
+
+function typeClassOf(value: string): TypeClass {
+  return value === null ? "null" : (typeof value as TypeClass);
+}
 
 export default GridScalar;
